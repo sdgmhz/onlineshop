08b8b7806edcae0c5e5e98303500f1db4f27a845
Sadegh Mehdizadeh <sdg.mhz@gmail.com>
2024-10-30 11:49:33 +0330

    add tests to app pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestPagesApp(TestCase):
+    def test_home_page_url(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Home Page")
+
+    def test_home_page_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, 'pages/home.html')
+
+    def test_about_us_page_url(self):
+        response = self.client.get("/aboutus/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_by_name(self):
+        response = self.client.get(reverse("aboutus"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page_content(self):
+        response = self.client.get(reverse("aboutus"))
+        self.assertContains(response, "About Us")
+
+    def test_about_us_page_template(self):
+        response = self.client.get(reverse("aboutus"))
+        self.assertTemplateUsed((response, 'pages/aboutus.html'))
